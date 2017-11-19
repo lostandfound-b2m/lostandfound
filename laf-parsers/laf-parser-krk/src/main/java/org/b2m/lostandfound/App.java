cf19c2e0b83466617eb21e09ed7a0174ccaf013a
Marta <pacuszka.m@gmail.com>
2017-11-20 00:25:17 +0100

    Class PdfRetriever modified - finds URL that is needed to parse PDF on Krk resource site

--- laf-parsers/laf-parser-krk/src/main/java/org/b2m/lostandfound/App.java
+++ laf-parsers/laf-parser-krk/src/main/java/org/b2m/lostandfound/App.java
@@ -1,6 +1,8 @@
 package org.b2m.lostandfound;
 
 import java.io.IOException;
+import java.text.ParseException;
+
 import org.apache.log4j.PropertyConfigurator;
 /**
  * Hello world!
@@ -9,10 +11,12 @@ import org.apache.log4j.PropertyConfigurator;
 public class App 
 {
 
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) throws IOException, ParseException {
         // Configure Log4J
-        PropertyConfigurator.configure(App.class.getClassLoader().getResource("log4j.properties"));
+        //PropertyConfigurator.configure(App.class.getClassLoader().getResource("log4j.properties"));
 
         PdfFile file=new PdfRetriever().retrieve("https://www.bip.krakow.pl/zalaczniki/dokumenty/n/189876/karta");
+        System.out.println("ok: " + file.getUrl());
+
     }
 }
--- laf-parsers/laf-parser-krk/src/main/java/org/b2m/lostandfound/PdfRetriever.java
+++ laf-parsers/laf-parser-krk/src/main/java/org/b2m/lostandfound/PdfRetriever.java
@@ -3,20 +3,45 @@ package org.b2m.lostandfound;
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.Objects;
 
+/*
+ * Class that finds URL to PDF file on Krakow L&F Office resources site,
+ * such as this one: https://www.bip.krakow.pl/zalaczniki/dokumenty/n/189876/karta,
+ * and retrieves all attributes of PdfFile object.
+ *
+ */
 public class PdfRetriever {
 
     private static Logger log = Logger.getLogger(PdfRetriever.class);
 
-    public PdfFile retrieve(String url) throws IOException {
+    public PdfFile retrieve(String url) throws IOException, ParseException {
         log.info("Retrieving HTML");
         Document doc=Jsoup.connect(url).get();
-        System.out.println(doc.title());
+        Element content = doc.getElementById("mainDiv");
 
-        return null;
+        PdfFile fileToBeFound=new PdfFile();
+
+        /* Attempt to find URL */
+        Elements urls = content.getElementsByTag("a");
+        for (Element urlToBeFound : urls)
+            if (Objects.equals(urlToBeFound.text(), "zobacz"))
+                fileToBeFound.setUrl("https://www.bip.krakow.pl" + urlToBeFound.attr("href"));
+
+
+        return fileToBeFound;
     }
 
-
+    private Date StringToDate(String strDate) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.parse(strDate);
+    }
 }
