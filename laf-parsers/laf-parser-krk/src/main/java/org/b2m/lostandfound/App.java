976b5a0676209664afd7c52f017d7615eaa3e1ff
Marta <pacuszka.m@gmail.com>
2017-11-20 22:47:45 +0100

    PdfRetriever().retrieve() return the list of all URLs to PDF files found on main page

--- laf-parsers/laf-parser-krk/src/main/java/org/b2m/lostandfound/App.java
+++ laf-parsers/laf-parser-krk/src/main/java/org/b2m/lostandfound/App.java
@@ -2,6 +2,7 @@ package org.b2m.lostandfound;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.List;
 
 import org.apache.log4j.PropertyConfigurator;
 /**
@@ -15,9 +16,12 @@ public class App
         // Configure Log4J
         //PropertyConfigurator.configure(App.class.getClassLoader().getResource("log4j.properties"));
 
-        PdfFile file=new PdfFile();
-        new PdfRetriever().retrieve("https://www.bip.krakow.pl/zalaczniki/dokumenty/n/189876/karta", file);
-        System.out.println("ok: " + file.getUrl());
+
+        List<PdfFile> files = new PdfRetriever().retrieve("https://www.bip.krakow.pl/?dok_id=19964");
+        for (PdfFile file : files) {
+            System.out.println(file.getUrl());
+        }
+
 
     }
 }
--- laf-parsers/laf-parser-krk/src/main/java/org/b2m/lostandfound/PdfRetriever.java
+++ laf-parsers/laf-parser-krk/src/main/java/org/b2m/lostandfound/PdfRetriever.java
@@ -9,21 +9,39 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Calendar;
-import java.util.Objects;
+import java.util.*;
 
 /*
- * Class that finds URL to PDF file on Krakow L&F Office resources site,
+ * Class that retrieves URLs to PDF files on Krakow L&F Office site.
+ * First, given main page address, it finds links to resource sites,
  * such as this one: https://www.bip.krakow.pl/zalaczniki/dokumenty/n/189876/karta,
- * and retrieves all attributes of PdfFile object.
+ * and then, retrieves from it all attributes of PdfFile objects and returns them as List.
  *
  */
 public class PdfRetriever {
 
     private static Logger log = Logger.getLogger(PdfRetriever.class);
 
-    public static void retrieve(String url, PdfFile fileToBeFound) throws IOException, ParseException {
+
+   public static List<PdfFile> retrieve(String url) throws IOException, ParseException {
+       List<PdfFile> resultList = new ArrayList<PdfFile>();
+       Document doc = Jsoup.connect(url).get();
+       Element content = doc.getElementById("mainDiv");
+
+       /* Looking for URLs with regex */
+       Elements links = content.getElementsByTag("a");
+       for (Element link : links) {
+           if (link.text().matches("Wykaz.*")) {
+               PdfFile file = new PdfFile();
+               file.setName(link.text());
+               retrieveUrl(link.attr("abs:href"),file);
+               resultList.add(file);
+           }
+       }
+       return resultList;
+   }
+
+    private static void retrieveUrl(String url, PdfFile fileToBeFound) throws IOException, ParseException {
         log.info("Retrieving HTML");
         Document doc=Jsoup.connect(url).get();
         Element content = doc.getElementById("mainDiv");
@@ -35,17 +53,20 @@ public class PdfRetriever {
                 fileToBeFound.setUrl(urlToBeFound.attr("abs:href"));
 
         /* Attempt to find last update's date */
+        Elements labels = content.select("div[class~=(metka_td)[12]]");
         Elements infos = content.getElementsByClass("metka_td3");
         int nr = 0;
-        for (Element info : infos) {
-            if (nr==5) {
-                fileToBeFound.setLastUpdate(StringToDate(infos.get(nr).text()));
+        for (Element label : labels) {
+            if (Objects.equals(label.text(), "Data aktualizacji:")) {
+                fileToBeFound.setLastUpdate(stringToDate(infos.get(nr).text()));
             }
+
             ++nr;
+
         }
     }
 
-    private static Date StringToDate(String strDate) throws ParseException {
+    private static Date stringToDate(String strDate) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return sdf.parse(strDate);
     }
