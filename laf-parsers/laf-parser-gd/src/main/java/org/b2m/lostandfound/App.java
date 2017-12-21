e6d7c6c4f3bda5a60b7c019b3327cf3ab3e0911d
mpac123 <pacuszka.m@gmail.com>
2017-12-21 14:03:47 +0100

    parser dla Gdanska: podstawowa funkcjonalnosc

--- laf-parsers/laf-parser-gd/src/main/java/org/b2m/lostandfound/App.java
+++ laf-parsers/laf-parser-gd/src/main/java/org/b2m/lostandfound/App.java
@@ -1,13 +1,16 @@
-package org.b2m.lostandfound.parser;
+package org.b2m.lostandfound;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws MalformedURLException {
         System.out.println( "Hello World!" );
+        ParserGd parser = new ParserGd(new URL("http://bip.gdansk.pl/urzad-miejski/Informacje-biura-rzeczy-znalezionych,a,50360"));
+        parser.getItemList();
     }
 }
---
+++ laf-parsers/laf-parser-gd/src/main/java/org/b2m/lostandfound/ParserGd.java
@@ -0,0 +1,82 @@
+package org.b2m.lostandfound;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ParserGd {
+
+    private Document document;
+
+    public ParserGd(URL url) {
+        try {
+            document = Jsoup.connect(url.toString()).get();
+        }
+        catch(IOException e) {
+            return;
+        }
+    }
+
+    public ParserGd(String string) {
+        File file = new File(string);
+        try {
+            document = Jsoup.parse(file, "UTF-8", "http://bip.gdansk.pl/urzad-miejski/");
+        }
+        catch(IOException e) {
+            return;
+        }
+    }
+
+    public List<Item> getItemList() {
+        List<Item> allItems = new LinkedList<>();
+        Elements content = document.getElementsByClass("article");
+        Elements rows = content.first().getElementsByTag("tr");
+        for(int i=0; i<3; i++) {
+            rows.remove(0);
+        }
+        for (Element row : rows) {
+            Elements fields = row.getElementsByTag("td");
+            if(fields.size()==5) {
+                fields.remove(0);
+                Date foundDate = getDate(fields.first().text());
+                fields.remove(0);
+                Date receiveDate = getDate(fields.first().text());
+                fields.remove(0);
+                String description = fields.first().text();
+                fields.remove(0);
+                String placeOfBeingFound = fields.first().text();
+                Item item = new Item(description,foundDate,receiveDate,null,placeOfBeingFound,"Gdańsk");
+                allItems.add(item);
+            }
+        }
+        System.out.println(allItems.get(2));
+        return allItems;
+    }
+
+    private Date getDate(String input) {
+        Date date;
+        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+        try {
+            date = df.parse(input);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            date = null;
+        }
+        return date;
+    }
+
+}
+
