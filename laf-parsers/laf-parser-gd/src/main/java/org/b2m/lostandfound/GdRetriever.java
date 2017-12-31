68b11465b018a6b750b28f196df0bed0abd02346
Marta <pacuszka.m@gmail.com>
2017-12-31 14:09:05 +0100

    GdRetriever finished

---
+++ laf-parsers/laf-parser-gd/src/main/java/org/b2m/lostandfound/GdRetriever.java
@@ -0,0 +1,43 @@
+package org.b2m.lostandfound;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GdRetriever implements Retriever{
+    public List<Item> retrieveItemsFromFiles(List<SourceFile> files) throws IOException {
+        List<Item> items = new ArrayList<>();
+        for (SourceFile file : files) {
+            items.addAll(new ParserGd(new URL(file.getUrl())).getItemList());
+        }
+        return items;
+    }
+
+    public List<SourceFile> retrieveFiles() {
+        String url = "http://bip.gdansk.pl/urzad-miejski/Informacje-biura-rzeczy-znalezionych,a,50360";
+        List<SourceFile> resultList = new ArrayList<>();
+        SourceFile file = new SourceFile();
+        file.setUrl(url);
+        file.setName("Gdańsk");
+        file.setOfficeName("Gdańsk");
+        Document doc = null;
+        /* updateChecker in this case is a hashCode of the content of the office's site */
+        try {
+        doc = Jsoup.connect(url).get();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        Elements content = doc.getElementsByClass("article");
+        file.setUpdateChecker(Integer.toString(content.hashCode()));
+
+        resultList.add(file);
+        return resultList;
+    }
+}
