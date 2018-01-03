c1b2e5318f81f8db53752f67970e027b9c9884fc
Marta <pacuszka.m@gmail.com>
2018-01-03 02:00:26 +0100

    New module: laf-scheduler. ItemListGetter moved to RegularUpdater in laf-scheduler, simpleListGetter moved to Servece in laf-service

--- laf-scheduler/src/main/java/org/b2m/lostandfound/RegularUpdater.java
+++ laf-scheduler/src/main/java/org/b2m/lostandfound/RegularUpdater.java
@@ -1,19 +1,20 @@
 package org.b2m.lostandfound;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.FileWriter;
+import javax.xml.transform.Source;
 import java.io.IOException;
-import java.io.Writer;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ItemListGetter {
 
-
-   /* public static void getList(Retriever retriever) throws IOException{
-        DaoService daoService = new DaoService();
-        List<SourceFile> files = retriever.retrieveFiles();
+public class RegularUpdater {
+    Retriever retriever;
+    Service daoService;
+    private RegularUpdater(Retriever retriever, Service daoService){
+        this.retriever=retriever;
+        this.daoService=daoService;
+    }
+    private void doScheduledUpdate() throws IOException{
+        List<SourceFile> filesFromRetriever = retriever.retrieveFiles();
         List<SourceFile> filesInDatabase = daoService.getSourceFiles(retriever.getOfficeName());
         List<SourceFile> newFiles = new ArrayList<>();
         List<SourceFile> filesToBeUpdated = new ArrayList<>();
@@ -29,7 +30,7 @@ public class ItemListGetter {
         //SourceFileDao was removed from office's site or updated
         for (SourceFile fileInDatabase : filesInDatabase) {
             isFileStillAvailable = false;
-            for (SourceFile file : files){
+            for (SourceFile file : filesFromRetriever){
                 if (file.getName().equals(fileInDatabase.getName())) {
                     if (!file.getUpdateChecker().equals(fileInDatabase.getUpdateChecker())) {
                         filesInDatabaseToBeUpdated.add(fileInDatabase);
@@ -46,7 +47,7 @@ public class ItemListGetter {
         }
 
         //SourceFileDao is listed on office's site but we don't have it in database yet
-        for (SourceFile file : files) {
+        for (SourceFile file : filesFromRetriever) {
             if (!filesInDatabase.contains(file)) {
                 newFiles.add(file);
             }
@@ -77,32 +78,6 @@ public class ItemListGetter {
         // New Items and new/modified SourceFiles added
         daoService.addSourceFiles(newFiles);
         daoService.addItems(itemsToAdd);
-        getJson(retriever.retrieveItemsFromFiles(files));
-    }*/
-
-    public static void simpleGetListAndSaveInJson(Retriever retriever) throws IOException {
-        List<SourceFile> files = retriever.retrieveFiles();
-        getJson(retriever.retrieveItemsFromFiles(files));
+        //getJson(retriever.retrieveItemsFromFiles(files));
     }
-
-    public static List<Item> simpleGetList(Retriever retriever) throws IOException {
-        List<SourceFile> files = retriever.retrieveFiles();
-        return retriever.retrieveItemsFromFiles(files);
-    }
-
-    public static List<Item> simpleGetListKrk() throws IOException {
-        KrkRetriever retriever = new KrkRetriever();
-        return simpleGetList(retriever);
-    }
-
-    private static void getJson(List<Item> items) throws IOException {
-        Writer writer = new FileWriter("Output.json");
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        gson.toJson(items, writer);
-
-        writer.close();
-    }
-
-
 }
--- laf-service/src/main/java/org/b2m/lostandfound/Service.java
+++ laf-service/src/main/java/org/b2m/lostandfound/Service.java
@@ -1,23 +1,53 @@
 package org.b2m.lostandfound;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.b2m.lostandfound.persist.*;
+
+import javax.xml.transform.Source;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.*;
 
-public class DaoService {
+public class Service {
 
     private static LostPropertyRepository lostPropertyRepository;
 
 
-    public DaoService() {
+    Service() {
         lostPropertyRepository = new LostPropertyDao();
     }
 
     //Date dateOfFound4 = new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime();
     //ItemDao myItem = new ItemDao("walizka", dateOfFound4, dateOfFound4, "00-753", "Warszawa", "Warszawa");
-    public ItemDao createNewItemDaoFromItem(Item itemFromParser){
+
+    /* Funkcje przepisujace obiekty z laf-parsers do obiektow w laf-persist */
+    ItemDao createNewItemDaoFromItem(Item itemFromParser){
         ItemDao newItemDao = new ItemDao(itemFromParser.getName(), itemFromParser.getFoundDate(), itemFromParser.getReceiveDate(), itemFromParser.getCityCode(), itemFromParser.getFoundPlace(), itemFromParser.getCityName());
         return newItemDao;
+    }
+
+    List<ItemDao> createNewItemDaoListFromItemList(List<Item> items) {
+        List<ItemDao> itemDaoList = new ArrayList<>();
+        for (Item item : items) {
+            itemDaoList.add(createNewItemDaoFromItem(item));
+        }
+        return itemDaoList;
+    }
+
+    /* Troche gowno, trzeba poprawic laf-paresrs */
+    SourceFileDao createNewSourceFileDaoFromSourceFile(SourceFile file) {
+        SourceFileDao fileDao = new SourceFileDao(file.getName(),file.getOfficeName(),file.getFileDate(),file.getUpdateChecker(),file.getUrl());
+        return fileDao;
+    }
+
+    List<SourceFileDao> createNewSourceFileDaoListFromSourceFileList(List<SourceFile> files) {
+        List<SourceFileDao> fileDaoList = new ArrayList<>();
+        for (SourceFile file : files) {
+            fileDaoList.add(createNewSourceFileDaoFromSourceFile(file));
+        }
+        return fileDaoList;
     }
     //temporarily without source file
     List<ItemDao> getRewritedItemsDaoFromParsedItems(LostPropertyOffice lostPropertyOffice,List<Item> items) throws IOException {
@@ -40,7 +70,9 @@ public class DaoService {
     }
 
 
-    void addSourceFiles(SourceFile sourceFile) {}
+    void addSourceFiles(SourceFile sourceFile) {
+        return  lostPropertyRepository.addSourceFile(createNewSourceFileDaoFromSourceFile(sourceFile));
+    }
 
     void addSourceFiles(List<SourceFile> sourceFileList) {
         for (SourceFile file : sourceFileList) {
@@ -49,7 +81,9 @@ public class DaoService {
     }
 
 
-    void deleteSourceFile(SourceFile sourceFile) {}
+    void deleteSourceFile(SourceFile sourceFile) {
+        return  lostPropertyRepository.deleteSourceFile(createNewSourceFileDaoFromSourceFile(sourceFile));
+    }
 
     void deleteSourceFile(List<SourceFile> sourceFileList) {
         for (SourceFile file : sourceFileList) {
@@ -58,7 +92,7 @@ public class DaoService {
     }
 
     List<Item> getItemsListedOnSourceFile(SourceFile sourceFile) {
-        return null;
+        return  lostPropertyRepository.getItemsListedOnSourceFile(createNewSourceFileDaoFromSourceFile(sourceFile));
     }
 
     List<Item> getItemsListedOnSourceFile(List<SourceFile> sourceFileList) {
@@ -69,15 +103,19 @@ public class DaoService {
         return items;
     }
 
-    void deleteItemsListedOnSourceFile(SourceFile sourceFile) {}
+    void deleteItemsListedOnSourceFile(SourceFile sourceFile) {
+        return  lostPropertyRepository.deleteItemsListedOnSourceFile(createNewSourceFileDaoFromSourceFile(sourceFile));
+    }
 
     void deleteItemsListedOnSourceFile(List<SourceFile> sourceFileList) {
         for (SourceFile file : sourceFileList) {
             deleteItemsListedOnSourceFile(file);
         }
     }
-
-    void addItems(List<ItemDao> itemList) {
+    void addItems(List<Item> itemList) {
+        addItemsDao(createNewItemDaoListFromItemList(itemList));
+    }
+    void addItemsDao(List<ItemDao> itemList) {
         lostPropertyRepository.addLostItems(itemList);
     }
 
@@ -85,7 +123,11 @@ public class DaoService {
         lostPropertyRepository.addLostPropertyOffice(office);
     }
 
-    void deleteItems(List<ItemDao> itemList) {
+    void deleteItems(List<Item> itemList) {
+        deleteItemsDao(createNewItemDaoListFromItemList(itemList));
+    }
+
+    void deleteItemsDao(List<ItemDao> itemList) {
         lostPropertyRepository.deleteLostItems(itemList);
     }
 
@@ -99,5 +141,45 @@ public class DaoService {
 
     List<ItemDao> returnAllItemsFromOffice(LostPropertyOffice office){
         return  lostPropertyRepository.returnAllItemsFromOffice(office);
+    }
+
+    List<ItemDao> findItems(String itemDescription, String cityName) {
+        return lostPropertyRepository.findByItemDescription(itemDescription,cityName);
+    }
+
+
+    /* A series of simpleGetList- functions that return List<Item> of all items
+     * found on given office's website or write this List to Json file
+     *
+     * NIE TYKAC MI TEGO!!!!!!! potem pewnie trzeba bedzie wywalic bo to bedzie gdzie indziej
+     * ale na razie zostawic
+     */
+    public void simpleGetListAndSaveInJson(Retriever retriever) throws IOException {
+        List<SourceFile> files = retriever.retrieveFiles();
+        getJson(retriever.retrieveItemsFromFiles(files));
+    }
+
+    public List<Item> simpleGetList(Retriever retriever) throws IOException {
+        List<SourceFile> files = retriever.retrieveFiles();
+        return retriever.retrieveItemsFromFiles(files);
+    }
+
+    public List<Item> simpleGetListKrk() throws IOException {
+        KrkRetriever retriever = new KrkRetriever();
+        return simpleGetList(retriever);
+    }
+
+    public List<Item> simpleGetListGd() throws IOException {
+        GdRetriever retriever = new GdRetriever();
+        return simpleGetList(retriever);
+    }
+
+    private void getJson(List<Item> items) throws IOException {
+        Writer writer = new FileWriter("Output.json");
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        gson.toJson(items, writer);
+
+        writer.close();
     }
 }
