35ba1df18896c3189ba29d35ca1c61f885b2220b
himeno61 <himen.binkiewicz@gmail.com>
2018-01-03 14:44:24 +0100

    RestApi repair

--- laf-restapi/src/main/java/org/b2m/lostandfound/RestApiController.java
+++ laf-restapi/src/main/java/org/b2m/lostandfound/RestApiController.java
@@ -2,6 +2,8 @@ package org.b2m.lostandfound;
 
 import org.b2m.lostandfound.persist.ItemDao;
 import org.b2m.lostandfound.persist.LostPropertyOffice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@SpringBootApplication
 public class RestApiController {
     List<ItemDao> findDaoList ;
     Service Service = new Service();
@@ -30,7 +33,7 @@ public class RestApiController {
         return findDaoList;
     }
     /** To call this request we have should use following path:
-       "http://localhost:8080/request?city="cityName
+       http://localhost:8080/request?city="cityName"
        This is GET method and will list all Items suited to your request
        with given city name from database
      */
@@ -44,4 +47,7 @@ public class RestApiController {
         return findDaoList;
     }
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RestApiController.class, args);
+    }
 }
