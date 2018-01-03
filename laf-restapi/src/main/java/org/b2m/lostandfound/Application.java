aed5db92c2d528fc31b665e8738a9f7d8b442506
himeno61 <himen.binkiewicz@gmail.com>
2018-01-03 02:27:36 +0100

    RestApi ready to work when Service will work

--- laf-restapi/src/main/java/org/b2m/lostandfound/Application.java
+++ laf-restapi/src/main/java/org/b2m/lostandfound/Application.java
@@ -8,7 +8,6 @@ import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         run(Application.class, args);
         System.out.printf(String.format("Application.main(%s)",Arrays.toString(args)));
--- laf-restapi/src/main/java/org/b2m/lostandfound/RestApiController.java
+++ laf-restapi/src/main/java/org/b2m/lostandfound/RestApiController.java
@@ -1,6 +1,7 @@
 package org.b2m.lostandfound;
 
 import org.b2m.lostandfound.persist.ItemDao;
+import org.b2m.lostandfound.persist.LostPropertyOffice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,7 @@ import java.util.List;
 public class RestApiController {
     List<ItemDao> findDaoList ;
     Service Service = new Service();
-    /* To call this request we have should use following path:
+    /** To call this request we have should use following path:
        http://localhost:8080/request?city="cityName"&desc="description"
        This is GET method and  will list all Items suited to your request
        with given city name and description from database
@@ -28,19 +29,19 @@ public class RestApiController {
         findDaoList = Service.findItems(descName,cityName);
         return findDaoList;
     }
-    /* To call this request we have should use following path:
+    /** To call this request we have should use following path:
        "http://localhost:8080/request?city="cityName
        This is GET method and will list all Items suited to your request
        with given city name from database
      */
-   /* @RequestMapping(value = "/request",
+   @RequestMapping(value = "/request",
             params = {"city"},
             method = RequestMethod.GET)
     public List<ItemDao> request(
             @RequestParam(value = "city") String cityName) {
         findDaoList = new ArrayList<>();
-        findDaoList = lostPropertyDao.returnAllItems(cityName);
+        findDaoList = Service.returnAllItemsFromOffice(cityName);
         return findDaoList;
-    }*/
+    }
 
 }
