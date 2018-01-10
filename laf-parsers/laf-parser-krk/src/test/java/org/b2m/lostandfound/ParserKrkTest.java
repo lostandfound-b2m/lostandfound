6331deb7d2cac4fc900b5b06c10aa042ad9e15ca
Marta <pacuszka.m@gmail.com>
2018-01-10 22:06:08 +0100

    KrkTest: tests only file from disk

--- laf-parsers/laf-parser-krk/src/test/java/org/b2m/lostandfound/ParserKrkTest.java
+++ laf-parsers/laf-parser-krk/src/test/java/org/b2m/lostandfound/ParserKrkTest.java
@@ -10,36 +10,17 @@ import static org.junit.Assert.*;
 
 public class ParserKrkTest {
     private List<Item> staticList = null;
-    private List<Item> webList = null;
 
-    @Before
+    @Test
     public void getItemListfromStatic() throws Exception {
         ParserKrk test = new ParserKrk("src/main/resources/2017_2.pdf");
         staticList= test.getItemList();
         for (Item item: staticList) System.out.println(item);
         assertNotNull("empty list",staticList);
+        assertEquals(staticList.get(0).getName(),"SASZETKA Z ZAW. ");
+        assertEquals(staticList.size(),799);
+        assertEquals(staticList.get(798).getCityCode(),"SA-03.5314.1.1457.2017");
     }
 
-    @Before
-    public void getItemListfromWeb() throws Exception {
-        KrkRetriever retriever = new KrkRetriever();
-        List<SourceFile> files = retriever.retrieveFiles();
-        SourceFile sourceFile = null;
-        for (SourceFile s :files) {
-            if(s.getName().equals("Wykaz 2017 r.")){
-                sourceFile = s;
-                break;
-            }
-        }
-        assertNotNull("no such file",sourceFile);
-        files = new LinkedList<>();
-        files.add(sourceFile);
-        webList = retriever.retrieveItemsFromFiles(files);
-        for (Item item: webList) System.out.println(item);
-    }
 
-    @Test
-    public void checkList() {
-        assertNotNull("lists are not the same",webList.equals(staticList));
-    }
 }
